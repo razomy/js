c80910b0934eb54f2f4535407ecdbc35784a6131
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-08-09 18:25:16 -0400

    fix date

--- src/dates.ts
+++ src/dates.ts
@@ -1,5 +1,5 @@
 export function isDateToday(inputDate: Date) {
-  const currentDate = new Date();
+  const currentDate = new Date(new Date().toISOString().split('T')[0]);
   const inputDateObj = new Date(inputDate);
 
   return (
