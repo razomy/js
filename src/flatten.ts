f32b3944e99412d2d30573481beaabe3c06bfd55
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2023-10-19 16:01:20 -0600

    add flatten

---
+++ src/flatten.ts
@@ -0,0 +1,49 @@
+export function flatten(data: Record<string, any>): Record<string, any> {
+  const result: Record<string, any> = {};
+
+  function recurse(cur: any, prop: string) {
+    if (typeof cur !== 'object' || cur === null) {
+      result[prop] = cur;
+    } else if (Array.isArray(cur)) {
+      for (let i = 0; i < cur.length; i++) {
+        recurse(cur[i], `${prop}[${i}]`);
+      }
+      if (cur.length === 0) {
+        result[prop] = [];
+      }
+    } else {
+      let isEmpty = true;
+      for (const p in cur) {
+        if (Object.prototype.hasOwnProperty.call(cur, p)) {
+          isEmpty = false;
+          recurse(cur[p], prop ? `${prop}.${p}` : p);
+        }
+      }
+      if (isEmpty && prop) {
+        result[prop] = {};
+      }
+    }
+  }
+
+  recurse(data, '');
+  return result;
+}
+
+export function unflatten(data: Record<string, any>): Record<string, any> {
+  if (typeof data !== 'object' || data === null || Array.isArray(data)) {
+    return data;
+  }
+  const regex = /\.?([^.[\]]+)|\[(\d+)]/g;
+  const resultholder: Record<string, any> = {};
+  for (const p in data) {
+    let cur = resultholder;
+    let prop = '';
+    let m;
+    while ((m = regex.exec(p))) {
+      cur = cur[prop] || (cur[prop] = m[2] ? [] : {});
+      prop = m[2] || m[1];
+    }
+    cur[prop] = data[p];
+  }
+  return resultholder[''] || resultholder;
+}
