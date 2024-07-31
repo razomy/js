7294ab223f9d030e6c384b92c3affc6986c97a3a
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2024-07-31 14:23:40 -0400

    cli call the code

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -4,7 +4,9 @@ export async function cli(terminalArgs: string[]): Promise<number> {
   }
   console.log('razomy.js.cli start')
 
-  const package_impl = await import('razomy.js.' + terminalArgs[0] + '/cli');
-  package_impl.cli(terminalArgs.slice(1));
+  const package_impl = await import('razomy.js.' + terminalArgs[0]);
+  package_impl[terminalArgs[1]](terminalArgs.slice(1));
+
+  console.log('razomy.js.cli finish')
   return 1;
 }
