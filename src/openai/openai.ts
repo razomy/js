359abb04ae47f9f5a129ef72f9c3c2882a85f000
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2024-08-22 08:45:01 -0400

    auth

--- src/openai/openai.ts
+++ src/openai/openai.ts
@@ -1,12 +1,8 @@
 import {OpenAI} from 'openai';
 
-export class openai {
-    openai: OpenAI;
-
-    constructor(ctx) {
-        this.openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
-
-    }
+export function openai(ctx: { openai: OpenAI }): { openai: OpenAI } {
+  ctx.openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  return ctx;
 }
