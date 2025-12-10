0333684cf6b3710884f5566fd64a0515ae702d61
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-12-10 18:31:56 -0500

    update

---
+++ src/fs/with.ts
@@ -0,0 +1,15 @@
+export interface WithDirPath {
+  dir_path: string;
+}
+
+export interface WithFileName {
+  file_name: string;
+}
+
+export interface WithFilePath {
+  file_path: string;
+}
+
+export interface WithSourcePath {
+  source_path: string;
+}
