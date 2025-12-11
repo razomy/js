81974b5e44d3934e4df1f694a1103ba15e10569a
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-12-11 18:34:31 -0500

    fix cli

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,19 +1,22 @@
-import {assign} from "razomy.js/key/assign";
-import {Module, TerminalArgs} from "razomy.js/cli/module";
-import {validateArrayStringThrow} from "razomy.js/cli/validateArrayStringThrow";
+import {assign} from 'razomy.js/key/assign';
+import {Module, TerminalArgs} from 'razomy.js/cli/module';
+import {validateArrayStringThrow} from 'razomy.js/cli/validateArrayStringThrow';
 
 export async function cli<
-    Rm extends keyof Module & string>(terminalArgs: TerminalArgs<Rm> | null | undefined): Promise<number> {
-    terminalArgs = validateArrayStringThrow(terminalArgs, "terminalArgs")
-    console.debug('razomy:js:cli:start')
+  Rm extends keyof Module & string>(terminalArgs: TerminalArgs<Rm> | null | undefined): Promise<number> {
+  terminalArgs = validateArrayStringThrow(terminalArgs, 'terminalArgs')
+  console.debug('razomy:js:cli:start')
 
-    const module_key = terminalArgs[0];
-    const value_arguments = terminalArgs.slice(1);
-    const path_key = module_key.replace(assign, '/');
-    const function_key = module_key.split(assign).at(-1)!;
-    const package_impl = await import('razomy.js' + path_key) as Module;
-    const result = await package_impl[function_key](...value_arguments);
+  const module_key_and_function_key = terminalArgs[0].split("/");
+  const value_arguments = terminalArgs.slice(1);
+  const module_key = module_key_and_function_key.join('/');
+  const function_key = module_key_and_function_key.slice(-1).join('/');
+  const package_impl = await import('razomy.js' + module_key) as Module;
+  const result = await package_impl[function_key](...value_arguments);
 
-    console.debug('razomy:js:cli:finish')
-    return result;
+  console.debug('razomy:js:cli:finish')
+  return result;
 }
+
+// cli('/console/log text'.split(' ')).then();
+// razomy /console/log text
