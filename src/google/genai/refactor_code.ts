c56b3f7ae69d2bbe613af7ed12706ef782b2f0a0
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-12-12 11:45:14 -0500

    add ai

---
+++ src/google/genai/refactor_code.ts
@@ -0,0 +1,50 @@
+import {GoogleGenAI} from '@google/genai';
+import {iterate} from 'razomy.js/fs/iterate';
+
+const ai = new GoogleGenAI({apiKey: ''});
+
+async function ask() {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: 'How does AI work?',
+  });
+  console.log(response.text);
+}
+
+function f(dir_path: string) {
+  iterate(dir_path, () => {
+
+  })
+}
+
+ask().then();
+
+const response = {
+  'candidates': [
+    {
+      'content': {
+        'parts': [
+          {
+            'text': '..........'
+          }
+        ],
+        'role': 'model'
+      },
+      'finishReason': 'STOP',
+      'index': 0
+    }
+  ],
+  'modelVersion': 'gemini-2.5-flash',
+  'usageMetadata': {
+    'promptTokenCount': 6,
+    'candidatesTokenCount': 1282,
+    'totalTokenCount': 2579,
+    'promptTokensDetails': [
+      {
+        'modality': 'TEXT',
+        'tokenCount': 6
+      }
+    ],
+    'thoughtsTokenCount': 1291
+  }
+}
