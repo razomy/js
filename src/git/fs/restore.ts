bb2e1e7432d985e85b0a5c611d38f3a0a01b833f
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2025-12-12 19:35:18 -0500

    add restore

---
+++ src/git/fs/restore.ts
@@ -0,0 +1,20 @@
+import {get_recursive_status} from 'razomy.js/git/fs/get_status';
+import {nodes_array_to_tree} from 'razomy.js/trees/list/nodes_array_to_tree';
+import {iterate_reverse} from 'razomy.js/trees/list/iterate';
+import {execSync} from 'node:child_process';
+import {SourcePath} from 'razomy.js/fs/path';
+
+function restore(source_path: SourcePath) {
+  const {files, repos} = get_recursive_status(source_path)
+  const tree = nodes_array_to_tree(repos, files);
+  iterate_reverse(tree[0], (node) => {
+    if (node.children.length !== 0) {
+      for (let child of node.children) {
+        const commitCommand = `git restore ` + child.value;
+        execSync(commitCommand, {cwd: node.value});
+      }
+    }
+  })
+}
+
+// restore('/Volumes/resource/resource/')
